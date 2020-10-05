8e88f44970d6ff6a153312df20b03bb37954fc33
emmyseco <emmyseco@gmail.com>
2020-10-04 20:13:25 -0500

    me funciona

---
+++ papel.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+onready var guiapanel = $"../CanvasLayer/guia"
+var isJugador = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(isJugador && Input.is_action_just_pressed("mouse")):
+		guiapanel.visible = true
+		get_tree().paused = true
+	if(isJugador && Input.is_action_just_pressed("ui_accept")):
+		guiapanel.visible = false
+		get_tree().paused = false
+	pass
+
+
+func _on_papel_body_entered(body):
+	if(body.name == 'PlayerLight'):
+		isJugador = true
+	pass # Replace with function body.
