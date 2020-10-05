cf8391c7ef01ebfcde45047d79493852d791e423
DarkDino <darkdinoxd@gmail.com>
2020-10-04 23:07:16 -0500

    tengo sueno

--- RealEnemy.gd
+++ RealEnemy.gd
@@ -67,4 +67,4 @@ func _physics_process(delta : float):
 
 func _on_Area2D_body_entered(body):
 	if(body.name == 'PlayerLight'):
-		get_tree().reload_current_scene()
+		get_tree().reload_current_scene()	
--- nodos/PlayerLight.gd
+++ nodos/PlayerLight.gd
@@ -4,6 +4,7 @@ onready var IsInterruptor = $"../isInterruptor"
 export var onInterruptor = false
 onready var tiempo = $"../Timer"
 onready var progreso = $"../CanvasLayer/BarraCansancio"
+onready var barraCansancio = $"../CanvasLayer/AnimatedSprite"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,56 +27,80 @@ var asustado = false
 
 #Valor en Porcentaje
 var cansancio = 100
-export var cansancioDescenso = 0.01
+export var cansancioDescenso = 0.015
 export var cansancioLimite = 40
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var anim = "normal"
+	var teclaPresionada = false
 	motion = Vector2()
 	IsInterruptor.set_text(str(onInterruptor))
 	motion.x = 0
 	motion.y = 0
+	if progreso.visible: asustado = true
 	
 	# Movimiento del Jugador
 	if tiempo.time_left <= 0:
 		if Input.is_key_pressed(KEY_W):
 			motion.y = -1
+			teclaPresionada = true
 		if Input.is_key_pressed(KEY_S):
 			motion.y = 1
+			teclaPresionada = true
 		if Input.is_key_pressed(KEY_A):
 			motion.x = -1
 			direction = 0
+			teclaPresionada = true
 		if Input.is_key_pressed(KEY_D):
 			motion.x = 1
 			direction = 1
+			teclaPresionada = true
 
-		if asustado: 
-			anim = "asustado"
-		else: anim = "normal"
-
-		if motion.x != 0 || motion.y != 0: 
-			_processDescanso()
-			if direction > 0:
-				$AnimatedSprite.play("derecha_anim_" + anim)
-			else:
-				$AnimatedSprite.play("izquierda_anim_" + anim)
+		if teclaPresionada: _processDescanso()
+		
+	if asustado: 
+		anim = "asustado"
+	else: anim = "normal"
+		
+	if teclaPresionada: 
+		if direction > 0:
+			$AnimatedSprite.play("derecha_anim_" + anim)
 		else:
-			if direction > 0:
-				$AnimatedSprite.play("derecha_" + anim)
-			else:
-				$AnimatedSprite.play("izquierda_" + anim)
+			$AnimatedSprite.play("izquierda_anim_" + anim)
+	else:
+		if direction > 0:
+			$AnimatedSprite.play("derecha_" + anim)
+		else:
+			$AnimatedSprite.play("izquierda_" + anim)
 	
 	#press to space when stay on interruptor
 	#if(Input.is_action_just_pressed("ui_accept") && onInterruptor == true && tiempo.time_left <= 0):
-	if(Input.is_action_just_pressed("mouse") && onInterruptor == true && tiempo.time_left <= 0):
-		tiempo.start()
-		progreso.value = 0
-		progreso.visible = true
+	if(Input.is_action_just_pressed("mouse")):
+		if isTriggerEnemy(): print("test" + str(range(1,32)))
+		if onInterruptor && tiempo.time_left <= 0:
+			tiempo.start()
+			progreso.value = 0
+			progreso.visible = true
 	
-	if progreso.visible == true:
-		progreso.value = 100 - (tiempo.time_left *100 / 5)
-	motion = move_and_slide(motion.normalized() * SPEED)
+	if progreso.visible:
+		progreso.value = 100 - (tiempo.time_left * 100 / 5)
+		
+	motion = move_and_slide(motion.normalized() * (SPEED * cansancio / 100.0))
+	
+	# Barra de Cansancio
+	barraCansancio.play("barra" + str(int(cansancio/10)-2))
+
+func isTriggerEnemy():
+	var mousePos = get_viewport().get_mouse_position()
+	var enemyPos = Vector2($"../RealEnemy".position.x-6,$"../RealEnemy".position.y-16)
+	var enemySize = Vector2(13,28)
+
+	if (mousePos.x >= enemyPos.x && mousePos.x <= enemyPos.x + enemySize.x):
+		if (mousePos.y >= enemyPos.y && mousePos.y <= enemyPos.y + enemySize.y):
+			return true
+	
+	return false
 
 func _processDescanso():
 	if cansancio > cansancioLimite:
