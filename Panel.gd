254e5a64007d455cb58f8b47e9719730d96f63b5
David <raymondinf23@gmail.com>
2020-10-05 22:33:24 -0400

    Linea de codigo innecesaria borrada

--- Panel.gd
+++ Panel.gd
@@ -7,9 +7,8 @@ extends Panel
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	$"../../MenuBackground/CanvasModulate".visible = true
-	
+#func _ready():
+#	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
